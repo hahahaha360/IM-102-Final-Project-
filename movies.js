359371ff57f51ce2781889347d6caffc87f7ec8b
Jake Bernal <jakebernal512@email.com>
2025-07-31 22:35:53 +0800

    the file is up to date

--- movies.js
+++ movies.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -15,21 +14,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-=======
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Middleware to parse JSON
-app.use(express.json());
-
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
-
-// Start server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
->>>>>>> 612e8486445bda4e7070d26cc046a1d2bfdc7017
